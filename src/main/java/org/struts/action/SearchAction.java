340565b71e90456eec817e790971b80cb5dddf26
atlantis0617 <1636720042@qq.com>
2018-05-30 12:06:06 +0800

    Struts2搜索Action

---
+++ src/main/java/org/struts/action/SearchAction.java
@@ -0,0 +1,15 @@
+package org.struts.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SearchAction extends ActionSupport {
+
+	private static final long serialVersionUID = -1253208014692295130L;
+
+	public String search() throws Exception {
+
+		return SUCCESS;
+
+	}
+
+}
